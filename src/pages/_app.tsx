bf1ead5e9fa8b96216eb41c44b4e6e81474bfa79
Ryota Ikezawa <pavegy@gmail.com>
2022-10-08 14:26:47 +0900

    :recycle: Resetting

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil"
+
+function App({ Component, pageProps }: AppProps): JSX.Element {
+
+	return (
+		<>
+			<RecoilRoot>
+				<Component {...pageProps} />;
+			</RecoilRoot>
+		</>
+	)
+}
+
+export default App;
